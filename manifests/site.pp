cf3323c052ac5ee608324c24bdf114b0425fe025
Ken Erwin <ken.erwin@kinneygroup.com>
2016-01-05 15:48:56 -0500

    Added code to eliminate annoying error

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,3 +28,12 @@ node 'dcgsmdc.searisc.dped.local' {
 }
 
 hiera_include('classes')
+
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
